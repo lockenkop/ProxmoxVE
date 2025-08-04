562226accb203c18496e519e2ae172736023d025
Jonas Fenchel <locken.kop@web.de>
2025-08-04 11:12:23 +0200

    init drop

---
+++ install/drop-install.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://adguard.com/
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Building Droplet"
+git clone https://github.com/Drop-OSS/droplet.git /opt/droplet && cd /opt/droplet
+yarn
+yarn build
+yarn link
+msg_ok "built Droplet"
+
+msg_info "Building Drop"
+git clone https://github.com/Drop-OSS/drop.git /opt/drop && cd /opt/drop
+yarn link "@drop/droplet"
+yarn
+yarn build
+msg_ok "built drop"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/AdGuardHome.service
+[Unit]
+Description=AdGuard Home: Network-level blocker
+ConditionFileIsExecutable=/opt/AdGuardHome/AdGuardHome
+After=syslog.target network-online.target
+
+[Service]
+StartLimitInterval=5
+StartLimitBurst=10
+ExecStart=/opt/AdGuardHome/AdGuardHome "-s" "run"
+WorkingDirectory=/opt/AdGuardHome
+StandardOutput=file:/var/log/AdGuardHome.out
+StandardError=file:/var/log/AdGuardHome.err
+Restart=always
+RestartSec=10
+EnvironmentFile=-/etc/sysconfig/AdGuardHome
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now AdGuardHome
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
